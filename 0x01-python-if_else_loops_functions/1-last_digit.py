11c40fe31057ed500c94ead7224acae6ae7617ee
ubonisrael <akpanudohubonisrael@gmail.com>
2023-10-31 13:56:10 -0700

    0x01 tasks 1 and 8 corrections

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,10 @@ if number > 0:
     d = number % 10
 else:
     d = (number * -1) % 10
-if d > 5:
-    print("{} and is greater than 5".format(d if number > 0 else -d))
-elif d < 6 and d > 0:
-    print("{} and is less than 6 and not 0".format(d if number > 0 else -d))
+
+if d == 0:
+    print("{} and is 0".format(d))
+elif d > 5 and number > 0:
+    print("{} and is greater than 5".format(d))
 else:
-    print(f"{last_digit} and is 0")
+    print("{} and is less than 6 and not 0".format(d if number > 0 else -d))

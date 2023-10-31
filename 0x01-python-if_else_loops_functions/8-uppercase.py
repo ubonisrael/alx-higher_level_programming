4659620f5a106287475391708fe8263dcdef0ad8
ubonisrael <akpanudohubonisrael@gmail.com>
2023-10-31 14:01:04 -0700

    0x01 task 8 corrections 2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """prints a string in uppercase followed by a new line"""
     for i in str:
         x = ord(i)
         if x >= 'a' and x <= 'z':
-            x = chr(ord(x) - ord(' '))
-        print("{}".format(x), end='')
+            x = chr(ord(x) - ord(" "))
+        print("{}".format(x), end="")
     print("")

deb0bc942be8e533e8f4de64911cb45807e87c43
ubonisrael <akpanudohubonisrael@gmail.com>
2023-11-04 12:22:41 -0700

    0x03 task 6 corrections

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
+        a = 0
         for y in x:
-            print("{:d}".format(y), end='')
+            print("{:d}".format(y), end=' ' if a < len(x) - 1 else '')
+            a += 1
         print()

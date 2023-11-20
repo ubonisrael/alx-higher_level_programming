1d986144fe6843723b1ed7262afe510e63269821
ubonisrael <akpanudohubonisrael@gmail.com>
2023-11-20 06:11:56 -0800

    0x05 task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    counter = 0;
+    for a in my_list:
+        try:
+            if counter == x:
+                break
+            print("{}".format(a), end="")
+            counter += 1
+        except IndexError:
+            print("Index not found")
+    print()
+    return counter

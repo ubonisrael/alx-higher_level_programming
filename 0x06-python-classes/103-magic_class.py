33ab3b77e2b2990eb16341d00f8bcbd080644f18
ubonisrael <akpanudohubonisrael@gmail.com>
2023-11-21 14:09:38 -0800

    0x06 task 103 corrections

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """Corresponding code for MagicClass Bytecode. """
-
+import dis
 
 class MagicClass:
     """ Initiates a class for a circle. """
-    def __init__(self, radius=0):
+    def __init__(self, radius):
         """Initializes the circles attributes. """
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self.__radius = radius
+        else:
+            self.__radius = radius
 
     def area(self):
         """ Calculates the area of the circle. """
@@ -17,3 +19,5 @@ class MagicClass:
     def circumference(self):
         """ Calculates the circumference of the circle. """
         return 2 * math.pi * self.__radius
+
+dis.dis(MagicClass)

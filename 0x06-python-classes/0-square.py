433421d337cdd0f996f6a4ae779e9e252197f3c3
ubonisrael <akpanudohubonisrael@gmail.com>
2023-11-21 06:38:27 -0800

    0x06 task 0 added module docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" This module creates an empty class that defines a square """
 class Square:
-    """ An empty class that deines a square."""
+    """ An empty class that defines a square."""
     pass

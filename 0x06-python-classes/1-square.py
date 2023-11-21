cf08296c55c16384f0978392e1954d61d28c6a91
ubonisrael <akpanudohubonisrael@gmail.com>
2023-11-21 06:47:31 -0800

    0x06 task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module creates a class that defines a square """
+
+
+class Square:
+    """ An empty class that defines a square."""
+    def __init__(self, size):
+        """ Initializes the square object to a size of 'size' """
+        self.__size = size

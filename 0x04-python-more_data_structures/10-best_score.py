3b95f5257d01ae2787cdefd54cd14cfc52880f00
ubonisrael <akpanudohubonisrael@gmail.com>
2023-11-22 00:12:00 -0800

    0x04 task 10 removed test print

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,5 @@ def best_score(a_dictionary):
     if a_dictionary is not None and len(a_dictionary) > 0:
         for k, v in a_dictionary.items():
             if v > (a_dictionary[max_score] if max_score is not None else 0):
-                print(k)
                 max_score = k
     return max_score

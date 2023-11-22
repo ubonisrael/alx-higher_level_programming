a4d95e6fcd190bd2d477794fcf3347298149fc15
ubonisrael <akpanudohubonisrael@gmail.com>
2023-11-22 06:10:08 -0800

    0x05 task 102 fixed typo

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / i
-        except as Exception:
+        except Exception:
             result = a + b
             break
     return result

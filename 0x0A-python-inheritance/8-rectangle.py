cd34df5daef5019b12d0d4923955cafb2d7eb631
ubonisrael <akpanudohubonisrael@gmail.com>
2023-12-04 08:26:09 -0800

    0x0A task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Contains the BaseGeometry class. """
+
+
+class BaseGeometry:
+    """ A class that defines a geometric shape. """
+
+    def area(self):
+        """ Raises an exception. """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """ Validates value of name attribute. """
+        if type(value) is not int:
+            raise TypeError('{:s} must be an integer'.format(name))
+        if value < 1:
+            raise ValueError('{:s} must be greater than 0'.format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ A class that defines a rectangular shape. """
+    def __init__(self, width, height):
+        """ Initializes the rectangle object. """
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
+        self.__width = width
+        self.__height = height

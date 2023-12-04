cfd3a6df3501d45659a57b059b6ff8dc54cb160a
ubonisrael <akpanudohubonisrael@gmail.com>
2023-12-04 08:30:33 -0800

    0x0A task 8 correction2

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
 """ Contains the BaseGeometry class. """
-
-
-class BaseGeometry:
-    """ A class that defines a geometric shape. """
-
-    def area(self):
-        """ Raises an exception. """
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """ Validates value of name attribute. """
-        if type(value) is not int:
-            raise TypeError('{:s} must be an integer'.format(name))
-        if value < 1:
-            raise ValueError('{:s} must be greater than 0'.format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):

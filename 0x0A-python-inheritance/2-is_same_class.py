b6dbcfae8b4de83d04a9011ccec6799fdd6cd3a0
ubonisrael <akpanudohubonisrael@gmail.com>
2023-12-04 06:56:53 -0800

    0x0A task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Contains the is_same_class function. """
+
+
+def is_same_class(obj, a_class):
+    """ Returns if 'obj' is an exactly instance of 'a_class'"""
+
+
+    return type(obj) == a_class

56cca922d267ec54f7297613d4cc7a027f2a176b
ubonisrael <akpanudohubonisrael@gmail.com>
2023-12-04 05:52:51 -0800

    0x0A task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" COntains a class that inherits from list. """
+
+
+class MyList(list):
+    """ Inherits from list and prints a sorted list. """
+
+    def print_sorted(self):
+        """ Prints a sorted list. """
+        print(sorted(self[:]))

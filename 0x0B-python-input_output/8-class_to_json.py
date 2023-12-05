da552f3cf389a5c47dcb2f0ee7ef7a0ba1d2bf17
ubonisrael <akpanudohubonisrael@gmail.com>
2023-12-05 07:56:41 -0800

    0x0B task 8 correction

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,6 +6,4 @@ def class_to_json(obj):
     """ Returns the dictionary description with simple data
     structure for JSON serialization of an object.
     """
-    import json
-
-    return json.dumps(obj.__dict__)
+    return obj.__dict__

ad39eb60d400f3890b222460add0b75ee0862ada
ubonisrael <akpanudohubonisrael@gmail.com>
2023-12-11 10:55:19 -0800

    0x0C added msg to typeerror in to_json_string method in base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,10 +27,11 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         if type(list_dictionaries) is not list:
-            raise TypeError
+            raise TypeError("list_dictionaries must be a list of dictionaries")
         for x in list_dictionaries:
             if type(x) is not dict:
-                raise TypeError
+                raise TypeError(
+                    "list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
 
     @classmethod

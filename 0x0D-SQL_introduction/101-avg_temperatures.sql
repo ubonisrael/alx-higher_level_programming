a2ebcc3ee91a26f4e5c0de3d5cf14e4b0563509d
ubonisrael <akpanudohubonisrael@gmail.com>
2024-01-16 22:33:11 +0100

    removed db import command

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
 -- Import in hbtn_0c_0 database this table dump: download
-source temperatures.sql ;
+-- source temperatures.sql ;
 SELECT city, AVG(value) AS avg_temp FROM 
     temperatures GROUP BY city ORDER BY avg_temp DESC;

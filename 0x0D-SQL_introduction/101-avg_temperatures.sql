99f82674e942b6da0542aa15703b86c340ad54cd
ubonisrael <akpanudohubonisrael@gmail.com>
2024-01-16 19:46:23 +0100

    added solution for task 101 - corrections

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
 -- Import in hbtn_0c_0 database this table dump: download
 -- Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
-SET autocommit=0 ; source temperatures.sql ; COMMIT;
+source temperatures.sql ;
 SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;

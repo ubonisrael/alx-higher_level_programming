924eb23fbc6b01fe5b39662c8f761a7d62d9222d
ubonisrael <akpanudohubonisrael@gmail.com>
2024-01-16 17:28:18 +0100

    added solution for task 9 - corrections

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,6 @@
 --  creates a table second_table in the database hbtn_0c_0
 CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score) VALUES(1, "John", 10);
+INSERT INTO second_table(id, name, score) VALUES(1, "John", 3);
+INSERT INTO second_table(id, name, score) VALUES(1, "John", 14);
+INSERT INTO second_table(id, name, score) VALUES(1, "John", 8);

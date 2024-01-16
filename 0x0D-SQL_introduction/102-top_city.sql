42dc8b9631a9614d77f25ad96c2e6411728e8689
ubonisrael <akpanudohubonisrael@gmail.com>
2024-01-16 22:38:14 +0100

    removed db import command from tasks 102 and 103

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
 -- Write a script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
-source temperatures.sql ;
+/* source temperatures.sql ; */
 SELECT city, AVG(value) AS avg_temp FROM 
     temperatures WHERE month BETWEEN 7 AND 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;

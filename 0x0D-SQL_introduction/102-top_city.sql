95f385e253b2516c5c5ead7a3fe65d6f78b293e3
ubonisrael <akpanudohubonisrael@gmail.com>
2024-01-16 19:52:35 +0100

    added solution for task 101 - corrections - 2

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
+-- Write a script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)

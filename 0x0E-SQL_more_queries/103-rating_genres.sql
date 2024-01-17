46adcd5e7edfa7963aaf32addd0a1111614e49f3
ubonisrael <akpanudohubonisrael@gmail.com>
2024-01-17 20:24:01 +0100

    added solution for task 103 changed as to uppercase

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,7 +2,7 @@
 -- Each record should display: tv_genres.name - rating sum
 -- Results must be sorted in descending order by their rating
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) as rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id

b3ef4088b5db96cfeb5421a2aec1adc8af11be5d
ubonisrael <akpanudohubonisrael@gmail.com>
2024-01-17 17:03:22 +0100

    added solution for task 13 - fixed bug when joining

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,12 +6,12 @@
 -- Results must be sorted in descending order by the number of shows linked
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
--- source hbtn_0d_tvshows.sql;
+source hbtn_0d_tvshows.sql;
 SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_of_shows
 FROM tv_genres
-INNER JOIN tv_shows
-ON tv_genres.id = tv_shows.id
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
 GROUP BY genre
 ORDER BY number_of_shows DESC;

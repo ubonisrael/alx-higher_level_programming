882e97598a5058a5016b7ccd7b2de5d9db497813
ubonisrael <akpanudohubonisrael@gmail.com>
2024-01-17 20:22:41 +0100

    added solution for task 103 removed unneccessary comment

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,6 @@
 -- Write a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 -- Each record should display: tv_genres.name - rating sum
 -- Results must be sorted in descending order by their rating
--- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) as rating
 FROM tv_genres

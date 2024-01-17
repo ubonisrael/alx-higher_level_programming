79a84a31cf53770b48e4d61d43d94c3e4c6a16cf
ubonisrael <akpanudohubonisrael@gmail.com>
2024-01-17 20:09:01 +0100

    added solution for task 102 - removed extra SELECT statement

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,13 +3,10 @@
 -- Results must be sorted in descending order by the rating
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT shows.title, combo.rating
+SELECT shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows AS shows
-INNER JOIN (
-    SELECT ratings.show_id, SUM(ratings.rate) as rating
-    FROM tv_show_ratings as ratings
-    GROUP BY ratings.show_id
-) AS combo
-ON shows.id = combo.show_id
-ORDER BY combo.rating DESC
+INNER JOIN tv_show_ratings
+ON shows.id =  tv_show_ratings.show_id
+GROUP BY shows.title
+ORDER BY rating DESC
 ;

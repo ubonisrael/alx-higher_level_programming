77bb242e2f24b6ff8d3c3af25f701c04913a2976
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-12 17:51:33 +0100

    initital submission

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing']
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < arr.length; i++) console.log(arr[i]);
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 if (typeof Number(process.argv[2]) === 'number' && !isNaN(Number(process.argv[2]))) {
-    const num = Number(process.argv[2]);
-    let square = ''
-    for (let i = 0; i < num; i++) {
-        for (let j = 0; j < num; j++) {
-            square += 'X'
-        }
-        if (i < num - 1) square += '\n'
+  const num = Number(process.argv[2]);
+  let square = '';
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      square += 'X';
     }
-    console.log(square);
+    if (i < num - 1) square += '\n';
+  }
+  console.log(square);
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }

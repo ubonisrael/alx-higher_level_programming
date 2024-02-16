03e3d14ad68dd7a7b356c151a5b6d38aefedc7c4
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-16 16:49:11 +0100

    completed task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 "A script that lists all states from the db passed to it"
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-
     mydb = MySQLdb.connect(host="localhost", port=3306,
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])

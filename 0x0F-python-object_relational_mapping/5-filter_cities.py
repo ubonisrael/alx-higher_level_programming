3f56f23adcf858b5e0ea65a78da61adfe6d9cad9
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-16 17:14:28 +0100

    completed tasks 5 and 6

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """ a script that lists all cities
 from the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-
     mydb = MySQLdb.connect(host="localhost", port=3306,
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
 
     cur = mydb.cursor()
 
-    query = "SELECT name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = %s) ORDER BY id"
+    query = "SELECT name FROM cities WHERE state_id = \
+        (SELECT id FROM states WHERE name = %s) ORDER BY id"
     cur.execute(query, (sys.argv[4],))
     rows = cur.fetchall()
     length = len(rows)

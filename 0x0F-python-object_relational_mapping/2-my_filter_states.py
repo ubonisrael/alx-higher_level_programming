9eaac4e4ab08990cd72e23403f815baadd601f08
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-16 17:11:23 +0100

    make corrections for task 2 - 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,16 +8,17 @@ import sys
 if __name__ == "__main__":
     try:
         mydb = MySQLdb.connect(host="localhost", port=3306,
-                           user=sys.argv[1], passwd=sys.argv[2],
-                           db=sys.argv[3])
+                               user=sys.argv[1], passwd=sys.argv[2],
+                               db=sys.argv[3])
     except MySQLdb.Error as e:
         print('{}'.format(e))
         sys.exit(1)
 
     cur = mydb.cursor()
 
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        sys.argv[4])
+    query = "SELECT * FROM states WHERE name = BINARY '{:s}' \
+        ORDER BY id ASC".format(
+            sys.argv[4])
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:

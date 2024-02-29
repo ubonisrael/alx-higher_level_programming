49ed42856d3e03f31ee75c723631cd0f0f6a0fc0
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-29 18:17:12 +0100

    0x10 tasks 4, 5 initial attempts

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Sends a header variable to the URL
+curl -sH "X-School-User-Id: 98" "$1"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sends data to an url
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" "$1"

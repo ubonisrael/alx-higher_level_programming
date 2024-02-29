e6a0116c2669d6c8e4ba48194385671fbd7debe3
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-29 18:33:05 +0100

    0x10 task 6 added docstring for function

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,7 @@
 # Finds the peak in an array
 
 def find_peak(list_of_integers):
+    """Finds the peak in an array"""
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
     return (max(list_of_integers))

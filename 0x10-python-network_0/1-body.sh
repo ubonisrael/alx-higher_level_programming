741c0fa5b83da585c68f1155067780067a57a864
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-29 18:20:21 +0100

    0x10 tasks 1 correction

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# A script that gets the content lenght of an url
+curl -sL "$1"
--- 0x10-python-network_0/1.body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# A script that sends a GET request to the URL, and displays the body of the response
-# curl -sI "$1" | grep -i 'Content-Length' | cut -d ' ' -f 2

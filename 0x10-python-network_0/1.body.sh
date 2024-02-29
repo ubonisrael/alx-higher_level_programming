f371494a0fc98529f25de52d9e946788d2af0fbc
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-29 17:25:50 +0100

    0x10 task 2 initial attempt

---
+++ 0x10-python-network_0/1.body.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# A script that gets the content lenght of an url
+# curl -sI "$1" | grep -i 'Content-Length' | cut -d ' ' -f 2
+curl -sI "$1" | head
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# A script that sends a DELETE request
+curl -s -X "Delete" "$1"

f858d36c431aa7be3c56f380db7dae3e570c18d0
ubonisrael <akpanudohubonisrael@gmail.com>
2024-02-29 19:16:45 +0100

    0x10 task 6 restructured algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Finds the peak in an array"""
 
+
 def find_peak(list_of_integers):
     """Finds the peak in an array"""
     if list_of_integers is None or len(list_of_integers) == 0:
@@ -13,10 +14,11 @@ def find_peak(list_of_integers):
         return max(list_of_integers)
 
     mid = int(size / 2)
+    left_max = find_peak(list_of_integers[:mid])
+    right_max = find_peak(list_of_integers[mid + 1:])
     peak = list_of_integers[mid]
-    if peak > list_of_integers[mid + 1] and peak > list_of_integers[mid - 1]:
+    if peak > left_max and peak > right_max:
         return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    if left_max > peak and left_max > right_max:
+        return left_max
+    return right_max

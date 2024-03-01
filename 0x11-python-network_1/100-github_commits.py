6bfcc827642b124dc8c99cbfd510cea8ffb73c1f
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-01 20:31:33 +0100

    0x11 task 100 initial attempt

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that takes your GitHub credentials
+(username and password) and uses the GitHub API
+to display your id"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10".\
+        format(argv[1], argv[2])
+    res = requests.get(url)
+    try:
+        res = res.json()
+        for i in range(10):
+            print("{}: {}".format(res[i]['sha'],
+                                  res[i]['commit']['author']['name']))
+    except KeyError as e:
+        print("None")

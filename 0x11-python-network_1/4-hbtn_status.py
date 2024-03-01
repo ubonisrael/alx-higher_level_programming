0c5746fb750fcae5fe81dd04d62826c7b46bc58b
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-01 18:57:48 +0100

    0x11 task 4 removed test code

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,6 @@ import requests
 
 if __name__ == "__main__":
     res = requests.get('https://alx-intranet.hbtn.io/status')
-    print(res)
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))

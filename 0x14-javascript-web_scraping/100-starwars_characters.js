c601c0caf44b9670efbdda4fa9a598e970b3b5f8
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-26 17:17:03 +0100

    0x14 - completed task 101

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,14 +7,14 @@ request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (error, 
   } else {
     const data = JSON.parse(body);
     data.characters.forEach(character => {
-        request(character, (error, response, body) => {
-            if (error) {
-                console.error(error)
-            } else {
-                const data = JSON.parse(body)
-                console.log(data.name);
-            }
-        })
-    })
+      request(character, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          const data = JSON.parse(body);
+          console.log(data.name);
+        }
+      });
+    });
   }
 });
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
+const util = require('util');
 
-request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (error, response, body) => {
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, async (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
     const data = JSON.parse(body);
-    data.characters.forEach(character => {
-        request(character, (error, response, body) => {
-            if (error) {
-                console.error(error)
-            } else {
-                const data = JSON.parse(body)
-                console.log(data.name);
-            }
-        })
-    })
+    const requestUtil = util.promisify(request);
+    for (const character of data.characters) {
+      try {
+        const res = await requestUtil(character);
+        const data = await JSON.parse(res.body);
+        console.log(data.name);
+      } catch (err) {
+        console.error(err);
+      }
+    }
   }
 });
